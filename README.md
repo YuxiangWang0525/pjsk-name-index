595d874c7480d716c9dbd051e545750bab3f670c
YuxiangWang_0525(John Frederick Wang) <66881174+YuxiangWang0525@users.noreply.github.com>
2024-06-20 14:18:16 +0800

    1.2.0版本 新增 sekai_index方法 完善文档

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pjsk-name-index 使用文档
-`pjsk-name-index` 是一个用于索引《Project Sekai》中角色等名称的库。该库包含角色的全名、简称以及团队名称的索引。
+`pjsk-name-index` 是一个用于索引《Project Sekai》(下简称“世界计划”)中角色等名称的库。该库包含了所有角色的全称、简称、社团名称、戏称等信息，并提供了一个易于使用的索引表，方便快速查找和转换名称。
 ## 安装
 在您的项目中安装 `pjsk-name-index`，您可以使用 npm 或 yarn 命令行工具：
 ```bash
@@ -10,55 +10,74 @@ npm install pjsk-name-index
 yarn add pjsk-name-index
 ```
 ## 引入
-您可以在 Node.js 环境或 Vue 项目中引入此库。
-### 在 Node.js 中引入
-```javascript
-const pjskIndex = require('pjsk-name-index');
-```
-### 在 Vue 中引入
-首先，在 Vue 组件中引入：
-```javascript
-import pjskIndex from 'pjsk-name-index';
-```
-然后，您可以在 Vue 组件的 `data`、`computed` 或方法中使用这些索引。
-## 列出所有名称
-要列出所有角色的全名，您可以使用 `full_name_cn` 数组：
-```javascript
-console.log(pjskIndex.full_name_cn);
-```
-要列出所有团队的名称，您可以使用 `team_name_cn` 数组：
-```javascript
-console.log(pjskIndex.team_name_cn);
-```
-## 使用简称转全称索引表
-该库提供了一个 `character_index` 对象，它将角色的简称映射到其全名。例如，要将 "ick" 转换为 "星乃一歌"，您可以使用：
-```javascript
-console.log(pjskIndex.character_index['ick']);
-// 输出: 星乃一歌
-```
-同样，`team_index` 对象可以将团队的简称映射到其全名。例如，要将 "vs" 转换为 "虚拟歌手"，您可以使用：
-```javascript
-console.log(pjskIndex.team_index['vs']);
-// 输出: 虚拟歌手
-```
-在 Vue 组件中，您可以在模板或方法中使用这些索引来显示全名：
-```vue
-<template>
-  <div>
-    <p>角色全名：{{ characterIndex['ick'] }}</p>
-    <p>团队全名：{{ teamIndex['vs'] }}</p>
-  </div>
-</template>
+您可以在使用Node.js的后端或使用任意前端框架的项目中引入 `pjsk-name-index`。
+以下使用Vue.js作为示例：
+(注:如无特别说明,下文的所有代码均使用Vue.js 3.x编写,且使用了Vue.js单文件组件(SFC)语法)
+
+### 引入库
+在Vue组件中，使用`import`语句引入`pjsk-name-index`库：
+``` vue
 <script>
-import pjskIndex from 'pjsk-name-index';
-export default {
-  data() {
-    return {
-      characterIndex: pjskIndex.character_index,
-      teamIndex: pjskIndex.team_index
-    };
-  }
-}
+import { full_name } from 'pjsk-name-index';
 </script>
 ```
-这样，您就可以方便地在 Vue 应用中使用 `pjsk-name-index` 库来处理《Project Sekai》中的角色和团队名称了。
+## 索引
+
+
+### 万能的sekai_index()方法
+#### 字段名表
+
+| 字段  | 字段含义                |
+| ------------ |---------------------|
+| full_name  | 社团英文名称/角色英文名称       |
+| full_name_cn  | 社团中文名称/角色中文名称 |
+|  simple_name | 社团简称/角色简称           |
+| joke_name_cn  | (仅角色)角色戏称(别称)       |
+| full_name_cn_official  | (仅社团) 社团中文官方名称      |
+
+
+`pjsk-name-index`库中提供了`sekai_index()`方法,该方法可以根据提供的参数快速索引到对应的角色的其他名称。  
+#### 用法:  
+`sekai_index('角色全称/团队全称','团队类型(team或individual 社团或个人)').期望字段`  
+#### 样例:
+``` js
+import { sekai_index } from 'pjsk-name-index';
+
+console.log(sekai_index("knd","individual").full_name_cn)
+
+// 输出结果: > 宵崎奏
+
+console.log(sekai_index("萌萌飞跃少女团",'team').full_name)
+// 输出结果: > MORE MORE JUMP!
+```
+### 手动索引
+
+#### 常量名表
+| 常量名称 | 类型  | 含义  |
+| ------------ | ------------ | ------------ |
+|  full_name |  列表 | 角色英文全名  |
+| full_name_cn  | 列表  | 角色中文全名  |
+| team_name  | 列表  |  社团英文全名 |
+| team_name_cn  |  列表 | 社团中文全名  |
+|  team_name_cn_official | 列表  |  中文官译社团全名 |
+| simple_name  | 列表  | 角色简称  |
+|  simple_team_name | 列表  | 团队简称  |
+| joke_name_cn  |  字典 |  角色戏称(别名) |
+| character_index  |  字典 |  依据简称手动匹配角色中文全名 |
+| team_index  | 字典  | 依据简称手动匹配社团中文全名  |
+| team_cn_official_index  | 字典  |  中文官译社团全名手动匹配社团中文全名 |
+| team_cn_index  | 字典  |  社团中文全名手动匹配中文官译社团全名 |
+
+#### 用法:
+`import { 需要的常量 } from 'pjsk-name-index';`
+
+#### 样例:
+``` js
+import { full_name_cn } from 'pjsk-name-index';
+
+console.log(full_name_cn[0])
+
+// 输出结果: > 初音未来
+```
+
+协议:MIT License
--- index.js
+++ index.js
@@ -1,23 +1,124 @@
-const full_name = ['Hatsune Miku','Kagamine Rin','Kagamine Len','Megurine Luka','MEIKO','KAITO','Hoshino Ichika','Tenma Saki','Mochizuki Honami','Hinomori Shiho','Hanasato Minori','Kiritani Haruka','Momoi Airi','Hinomori Shizuku','Azusawa Kohane','Shiraishi An','Shinonome Akito','Aoyagi Toya','Tenma Tsukasa','Otori Emu','Kusanagi Nene','Kamishiro Rui','Yoisaki Kanade','Asahina Mafuyu','Shinonome Ena','Akiyama Mizuki']
-const full_name_cn = ['初音未来','镜音铃','镜音连','巡音流歌','MEIKO','KAITO','星乃一歌','天马咲希','望月穗波','日野森志步','花里实乃理','桐谷遥','桃井爱莉','日野森雫','小豆泽心羽','白石杏','东云彰人','青柳冬弥','天马司','凤笑梦','草薙宁宁','神代类','宵崎奏','朝比奈真冬','东云绘名','晓山瑞希']
+const full_name = ['Hatsune Miku','Kagamine Rin','Kagamine Len','Megurine Luka','MEIKO','KAITO','Hoshino Ichika','Tenma Saki','Mochizuki Honami','Hinomori Shiho','Hanasato Minori','Kiritani Haruka','Momoi Airi','Hinomori Shizuku','Azusawa Kohane','Shiraishi An','Shinonome Akito','Aoyagi Toya','Tenma Tsukasa','Otori Emu','Kusanagi Nene','Kamishiro Rui','Yoisaki Kanade','Asahina Mafuyu','Shinonome Ena','Akiyama Mizuki'];
+const full_name_cn = ['初音未来','镜音铃','镜音连','巡音流歌','MEIKO','KAITO','星乃一歌','天马咲希','望月穗波','日野森志步','花里实乃理','桐谷遥','桃井爱莉','日野森雫','小豆泽心羽','白石杏','东云彰人','青柳冬弥','天马司','凤笑梦','草薙宁宁','神代类','宵崎奏','朝比奈真冬','东云绘名','晓山瑞希'];
 
-const team_name = ['VIRTUAL SINGER','Leo/need','MORE MORE JUMP!','Vivid BAD SQUAD','Wonderlands x Showtime','Nightcord at 25:00']
-const team_name_cn = ['虚拟歌手','Leo/need','MORE MORE JUMP!','Vivid BAD SQUAD','Wonderlands x Showtime','25时，在Nightcord。']
-const team_name_cn_official = ['虚拟歌手','狮雨星绊','萌萌飞跃少女团','炫狂小队','奇幻仙境演出秀','25点,夜音见']
+const team_name = ['VIRTUAL SINGER','Leo/need','MORE MORE JUMP!','Vivid BAD SQUAD','Wonderlands x Showtime','Nightcord at 25:00'];
+const team_name_cn = ['虚拟歌手','Leo/need','MORE MORE JUMP!','Vivid BAD SQUAD','Wonderlands x Showtime','25时，在Nightcord。'];
+const team_name_cn_official = ['虚拟歌手','狮雨星绊','萌萌飞跃少女团！','炫狂小队','奇幻仙境演出秀','25点,夜音见'];
 
-const simple_name = ['miku','rin','len','luka','meiko','kaito','ick','saki','hnm','shiho','mnr','hrk','airi','szk','khn','an','akt','toya','tks','emu','nene','rui','knd','mfy','ena','mzk']
-const simple_team_name = ['vs','ln','mmj','vbs','ws','25时']
-
+const simple_name = ['miku','rin','len','luka','meiko','kaito','ick','saki','hnm','shiho','mnr','hrk','airi','szk','khn','an','akt','toya','tks','emu','nene','rui','knd','mfy','ena','mzk'];
+const simple_team_name = ['vs','ln','mmj','vbs','ws','25时'];
+//待补全,所有戏称/别名必须在字典的嵌套列表中
+const joke_name_cn = {
+    'Hatsune Miku':['葱','猫葱','白葱'],
+    'Kagamine Rin':[],
+    'Kagamine Len':[],
+    'Megurine Luka':[],
+    'MEIKO':[],
+    'KAITO':[],
+    'Hoshino Ichika':[],
+    'Tenma Saki':['马晓希'],
+    'Mochizuki Honami':['穗波妈妈'],
+    'Hinomori Shiho':[],
+    'Hanasato Minori':[],
+    'Kiritani Haruka':[],
+    'Momoi Airi':[],
+    'Hinomori Shizuku':[],
+    'Azusawa Kohane':[],
+    'Shiraishi An':[],
+    'Shinonome Akito':[],
+    'Aoyagi Toya':[],
+    'Tenma Tsukasa':[],
+    'Otori Emu':['汪大吼'],
+    'Kusanagi Nene':[],
+    'Kamishiro Rui':[],
+    'Yoisaki Kanade':['小气走'],
+    'Asahina Mafuyu':['马福友'],
+    'Shinonome Ena':['董慧敏'],
+    'Akiyama Mizuki':[]
+};
 // 生成索引
 let character_index = {};
 simple_name.forEach((name, index) => {
     character_index[name] = full_name_cn[index];
 });
 
-const team_index = {};
+let team_index = {};
 simple_team_name.forEach((name, index) => {
     team_index[name] = team_name_cn[index];
 });
+
+let team_cn_official_index = {};
+team_name_cn_official.forEach((name, index) => {
+    team_cn_official_index[name] = team_name_cn[index];
+})
+
+let team_cn_index = {};
+team_name_cn.forEach((name, index) => {
+    team_cn_index[name] = team_name_cn_official[index];
+})
+
+function sekai_index(name,type) {
+    function createIndex(data) {
+        const groupedData = {
+            teams: [],
+            individual: []
+        };
+
+        // 遍历团队数据并添加到groupedData中
+        data.team_name.forEach((team, index) => {
+            groupedData.teams.push({
+                full_name: team,
+                full_name_cn: data.team_name_cn[index],
+                full_name_cn_official: data.team_name_cn_official[index],
+                simple_name: data.simple_team_name[index]
+            });
+        });
+
+        // 遍历个人数据并添加到groupedData中
+        data.full_name.forEach((name, index) => {
+            const member = {
+                full_name: name,
+                full_name_cn: data.full_name_cn[index],
+                simple_name: data.simple_name[index],
+                joke_name_cn: data.joke_name_cn[name] ? data.joke_name_cn[name].join(',') : ''
+            };
+            groupedData.individual.push(member);
+        });
+
+        return groupedData;
+    }
+    const data = {
+        full_name: full_name,
+        full_name_cn: full_name_cn,
+        simple_name: simple_name,
+        joke_name_cn: joke_name_cn,
+        team_name: team_name,
+        team_name_cn: team_name_cn,
+        team_name_cn_official: team_name_cn_official,
+        simple_team_name: simple_team_name
+    };
+    const index = createIndex(data);
+
+
+    let result;
+    if (type === 'team') {
+        result = index.teams.find(team => {
+            return Object.values(team).some(value =>
+                String(value).toLowerCase().includes(name.toLowerCase())
+            );
+        });
+    } else if (type === 'individual') {
+        result = index.individual.find(member => {
+            return Object.values(member).some(value =>
+                String(value).toLowerCase().includes(name.toLowerCase())
+            );
+        });
+    }
+    if (result === undefined){
+        result = null;
+    }
+    return result;
+}
 
 export {
     full_name,
@@ -28,5 +129,9 @@ export {
     simple_name,
     simple_team_name,
     character_index,
-    team_index
+    team_index,
+    joke_name_cn,
+    team_cn_official_index,
+    team_cn_index,
+    sekai_index
 };
